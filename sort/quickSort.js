9538b2fa7162aec880b1d1525fdcdfffad98b06d
이신일 <itsinil@gmail.com>
2019-11-20 00:03:50 +0900

    quickSort 테스트

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,16 +1,17 @@
 //1. 정렬할 리스트에 피벗 지점을 정한다. (보통 중간)
 //2. 피벗의 값을 기준으로 start, end 값을 정한다.
-//3. start 와 end가 서로 교차할때까지 피벗의 값의 크기에 따라 반복하여 스왑한다.
+//3. start 와 end가 피벗의 값을 교차할때까지 피벗의 값을 기준으로 start,end 값을 swap 한다.
+//4. partition를 진행한 후 해당 값을 기준으로 나눈 리스트를 각각 sort 재귀함수를 호출 한다.
 function quickSort(list) {
     return sort(list, 0, list.length-1)
 }
 
-
 function sort(list, start, end) {
-    var part = partition(list, start, end)
+    var part = partition(list, start, end);
     if( start < part-1 ){
         list = sort(list, start, part-1)
-    }else if(part <  end ){
+    }
+    if( part < end ){
         list = sort(list, part, end)
     }
     return list
@@ -20,18 +21,18 @@ function partition(list, start, end) {
     var pivot = parseInt((start+end) / 2)
 
     while (start <= end){
-        while (list[pivot] > list[start]) start++
-        while (list[pivot] < list[end]) end--
-
-        if(start <= end){
-            swap(list, start, end);
-            start++
-            end++
+        while (list[pivot] > list[start]) start++;
+        while (list[pivot] < list[end]) end--;
+        if(start > end){
+            break;
         }
+        swap(list, start, end);
+        start++
+        end--
     }
+
     return start
 }
-//1 2 5 4 3
 
 function swap(list, start, end){
     var temp = list[start];
@@ -39,7 +40,6 @@ function swap(list, start, end){
     list[end] = temp
 
 }
-
 
 export {
     quickSort
--- sort/sort.test.js
+++ sort/sort.test.js
@@ -2,20 +2,19 @@ import { selectSort, bubbleSort } from './basicSort'
 import { quickSort } from './quickSort'
 
 test('Bubble Sort Test', () => {
-    let ramdomList = [6, 2, 5,  1, 100, 10];
-    let sortedList = bubbleSort(ramdomList)
+    let randomList = [666, 6, 2, 5,  1, 100, 10, 111, 12, 50, ];
+    let sortedList = bubbleSort(randomList)
     console.log(sortedList)
 });
 
-
 test('Select Sort Test', () => {
-    let ramdomList = [6, 2, 5,  1, 100, 10];
-    let sortedList = selectSort(ramdomList)
+    let randomList = [666, 6, 2, 5,  1, 100, 10, 111, 12, 50, ];
+    let sortedList = selectSort(randomList)
     console.log(sortedList)
 });
 
 test('Quick Sort Test', () => {
-    let ramdomList = [6, 2, 5,  1, 100, 10];
-    let sortedList = quickSort(ramdomList)
+    let randomList = [666, 6, 2, 5,  1, 100, 10, 111, 12, 50, ];
+    let sortedList = quickSort(randomList)
     console.log(sortedList)
 });
